2cd95aa485fbc0b2941c1d2b48451280d8518e72
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-04-06 21:29:43 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,8 @@
 import { motion } from "framer-motion";
-import { technologies } from "../constants";
 import { styles } from "../styles";
-import { ComputersCanvas, BallCanvas } from "./canvas";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  const icons = technologies.map((tech) => tech.icon);
-
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -28,11 +25,6 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
-
-      <div className="absolute top-[400px] w-full flex justify-center items-center">
-        <BallCanvas icons={icons} />
-      </div>
-
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const icons = technologies.map((tech) => tech.icon);
   return (
     <>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+            <BallCanvas icons={icons} />
           </div>
         ))}
       </div>
