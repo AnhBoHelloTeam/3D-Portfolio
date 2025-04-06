eaef095397e8bb55eb773a4c57a9588fc4e1f716
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-04-06 20:19:26 +0530

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,27 @@
-import { BallCanvas } from "./canvas";
+import { Ball } from "./canvas"; // Import Ball instead of BallCanvas
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload } from "@react-three/drei";
+import CanvasLoader from "./Loader";
 
 const Tech = () => {
   return (
-    <>
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
+    <div className="flex justify-center">
+      <Canvas
+        frameloop="demand"
+        dpr={[1, 2]}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 0, 1]} />
+        {technologies.map((technology, index) => (
+          <Ball key={index} imgUrl={technology.icon} />
         ))}
-      </div>
-    </>
+        <Preload all />
+      </Canvas>
+    </div>
   );
 };
 
