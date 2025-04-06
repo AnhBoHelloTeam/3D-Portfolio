e148546a1f86431bb4111f8f1c8c3cac0a293744
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-04-06 21:57:25 +0530

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -81,10 +81,10 @@ const technologies = [
     name: "React JS",
     icon: reactjs,
   },
-  // {
-  //   name: "Redux Toolkit",
-  //   icon: redux,
-  // },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
   // {
   //   name: "Tailwind CSS",
   //   icon: tailwind,
