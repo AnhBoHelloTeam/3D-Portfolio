344e367d60726c27e7bb369f336a91cf05ddd680
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-04-07 14:14:28 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-justify break-all"
       >
         I'm a skilled software developer with a strong foundation in PHP,
         TypeScript, and JavaScript, and hands-on expertise in popular frameworks
